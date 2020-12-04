7007b2064e5cc32e083f6f01c3aa9112fcf9bb2f
Bing <bing_yu2001@163.com>
2020-12-04 11:00:53 +0800

    讲师CRUD

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -48,12 +48,48 @@ export default {
     }
   },
   created() {
-
+    // 判断路径有id值,做修改
+    if (this.$route.params && this.$route.params.id) {
+      // 从路径获取id值
+      const id = this.$route.params.id
+      // 调用根据id查询的方法
+      this.getInfo(id)
+    } else { // 路径没有id值，做添加
+      // 清空表单
+      this.teacher = {}
+    }
   },
   methods: {
+    // 根据讲师id查询的方法
+    getInfo(id) {
+      teacherApi.getTeacherInfo(id)
+        .then(response => {
+          this.teacher = response.data.teacher
+        })
+    },
     saveOrUpdate() {
-      // 调用添加讲师方法
-      this.saveTeacher()
+      // 判断修改还是添加
+      // 根据teacher是否有id
+      if (!this.teacher.id) {
+        // 添加
+        this.saveTeacher()
+      } else {
+        // 修改
+        this.updateTeacher()
+      }
+    },
+    // 修改讲师的方法
+    updateTeacher() {
+      teacherApi.updateTeacherInfo(this.teacher)
+        .then(response => {
+          // 提示信息
+          this.$message({
+            type: 'success',
+            message: '修改成功!'
+          })
+          // 回到列表页面 路由跳转
+          this.$router.push({ path: '/teacher/table' })
+        })
     },
     // 添加讲师的方法
     saveTeacher() {
